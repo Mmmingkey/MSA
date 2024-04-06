b20cbc5c075c0d2ff8e02326936ab14bdf04b0c6
tjals9139@naver.com <tjals9139@naver.com>
2024-04-06 14:59:51 +0900

    240406 commit

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,16 +1,26 @@
 package com.example.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String INSTANCE_ID;
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -26,7 +36,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from First Service";
+    public String check(HttpServletRequest request){
+        log.info("server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service %s, %s"
+                , env.getProperty("local.server.port")
+                , INSTANCE_ID);
     }
 }
